b3db7091f383467de17f8a0e792428a41d7f58d8
Ivan Ustyantsev <vangold915@gmail.com>
2023-10-12 22:47:27 +0700

    Footer and Header components completed

--- project-vue-cli/src/App.vue
+++ project-vue-cli/src/App.vue
@@ -1,13 +1,22 @@
 <template>
-  <HomepageComponent/>
+  <HeaderComponent />
+  <!-- <HomepageComponent /> -->
+  <ArticlesAndNewsComponent />
+  <FooterComponent />
 </template>
 
 <script>
 import HomepageComponent from './components/HomepageComponent.vue';
+import ArticlesAndNewsComponent from './components/ArticlesAndNewsComponent.vue';
+import HeaderComponent from './components/HeaderComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
 export default {
   name: 'App',
   components: {
+    HeaderComponent,
+    FooterComponent,
     HomepageComponent,
+    ArticlesAndNewsComponent,
   }
 }
 </script>
---
+++ project-vue-cli/src/components/HeaderComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="header center">
+    <a href="#" class="header__link-logo">
+      <img
+        :src="require('@/assets/img/homepage/Logo.svg')"
+        alt="logo"
+        class="header__logo"
+      />
+    </a>
+    <nav class="header__nav">
+      <a href="#" class="header__nav-link" v-for="(navName, index) in navNames" :key="index">{{ navName }}</a>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            navNames: ['Home', 'Project', 'Blog']
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
