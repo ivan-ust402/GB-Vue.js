f3b3941a473ef9011b736e0fceebad83351944ef
Ivan Ustyantsev <vangold915@gmail.com>
2023-10-12 22:21:07 +0700

    Homepage refactored to Vue cli project

--- project-vue-cli/src/App.vue
+++ project-vue-cli/src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HomepageComponent/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HomepageComponent from './components/HomepageComponent.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomepageComponent,
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
