8b53e666d105b96e11c0764e1ce645fe65756813
Ivan Ustyantsev <vangold915@gmail.com>
2023-10-20 22:00:14 +0700

    Completed default rectangle button

--- project-vue-cli-refactoring/src/App.vue
+++ project-vue-cli-refactoring/src/App.vue
@@ -1,26 +1,30 @@
 <template>
   <CircleButtonComponent />
   <PaginationButtonComponent :value="10" />
+  <DefaultRectangleButtonComponent :value="'Get started'" />
+  
 </template>
 
 <script>
 import CircleButtonComponent from "./components/buttons/CircleButtonComponent.vue"
+import DefaultRectangleButtonComponent from "./components/buttons/DefaultRectangleButtonComponent.vue";
 import PaginationButtonComponent from "./components/buttons/PaginationButtonComponent.vue"
 export default {
   name: "App",
   components: {
     CircleButtonComponent,
     PaginationButtonComponent,
-  },
+    DefaultRectangleButtonComponent
+},
 }
 </script>
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=DM+Serif+Display&family=Jost:wght@400;500;600&display=swap');
 
-$colorPrimary1: #cda274;
-$colorPrimary2: #292f36;
-$colorPrimary3: #f4f0ec;
+// $colorPrimary1: #cda274;
+// $colorPrimary2: #292f36;
+// $colorPrimary3: #f4f0ec;
 * {
   margin: 0;
   padding: 0;
---
+++ project-vue-cli-refactoring/src/components/buttons/DefaultRectangleButtonComponent.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <button class="rectangle-button">
+      <p class="rectangle-button__text">{{ value }}</p>
+      <img
+        :src="require('@/assets/img/buttons/Arrow.svg')"
+        class="rectangle-button__image"
+      />
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: () => "Default button",
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.rectangle-button {
+  color: #fff;
+  text-align: center;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 125%; /* 22.5px */
+  letter-spacing: 0.36px;
+  border-radius: 18px;
+  background: #292f36;
+  box-shadow: 0px 10px 20px 0px rgba(192, 192, 192, 0.35);
+  padding: 26px 49px 26px 48px;
+  width: auto;
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  transition: all 0.3s ease-in;
+  &:hover {
+    color: #cda274;
+    background-color: #151616;
+  }
+}
+
+
+</style>
