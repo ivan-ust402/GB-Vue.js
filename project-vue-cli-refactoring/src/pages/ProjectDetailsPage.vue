742f55de350738457c8fd83bea88a5427e574cb1
Ivan Ustyantsev <vangold915@gmail.com>
2023-10-25 00:06:06 +0700

    Text content added

--- project-vue-cli-refactoring/src/pages/ProjectDetailsPage.vue
+++ project-vue-cli-refactoring/src/pages/ProjectDetailsPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="project-details">
     <IntroBlock
       :style="{
         backgroundImage:
@@ -9,15 +9,58 @@
       }"
       :settings="{ hasTitle: false }"
     />
+    <div class="project-details__content center">
+      <div class="project-details__text-block">
+        <h2 class="project-details__title">{{ getTitle }}</h2>
+        <p class="project-details__text" v-html="getHTMLText"></p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import IntroBlock from "@/blocks/IntroBlock.vue"
+import { mapGetters } from "vuex"
 
 export default {
   components: { IntroBlock },
+  computed: {
+    ...mapGetters(["getTitle", "getHTMLText", "getImgSrcArr"]),
+  },
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.project-details {
+  &__content {
+    padding-top: 174px;
+    padding-bottom: 108px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  &__text-block {
+    display: flex;
+    flex-direction: column;
+    gap: 11px;
+    width: 658px;
+    padding-bottom: 70px;
+  }
+  &__title {
+    color: #292f36;
+    font-size: 50px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 125%; /* 62.5px */
+    letter-spacing: 1px;
+  }
+  &__text {
+    color: #4d5053;
+    font-size: 22px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 150%; /* 33px */
+    letter-spacing: 0.22px;
+  }
+}
+</style>
--- project-vue-cli-refactoring/src/store/modules/project-details-store.js
+++ project-vue-cli-refactoring/src/store/modules/project-details-store.js
@@ -4,7 +4,7 @@ export default {
             projectContent: [
                 {
                     title: 'Minimal Look Bedrooms',
-                    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquamsem vitae turpis dignissim maximus. Aliquam sollicitudin tellumassa, vbel maximus purus posuere in. Dojrices gravida dignissim. Praesent at nibh in mi fringilla mattis. Phasellus ut dolor odio. Aenean in the ipsum vel lectus bibendum commodo. 
+                    htmlText: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquamsem vitae turpis dignissim maximus. Aliquam sollicitudin tellumassa, vbel maximus purus posuere in. Dojrices gravida dignissim. Praesent at nibh in mi fringilla mattis. Phasellus ut dolor odio. Aenean in the ipsum vel lectus bibendum commodo. 
                     <br><br>
                     In nec sem suscipit, convallis leo vitae, lacinia nibh. Cras amet tellus lectus. Vivamus ipsum nunc, mattis quis nibh id, pellentesque arcu. Donec a pellentesque Cras erat enim, gravida non ante vitae,elequis convallis elit, in viverra felis. Donec ultrices tellus vitae iaculisvd porta. Proin tincidunt ligula id purus porttitor.`,
                     imgSrcArr: [
@@ -26,7 +26,15 @@ export default {
 
     },
     getters: {
-
+        getTitle(state) {
+            return state.projectContent[0].title;
+        },
+        getHTMLText(state) {
+            return state.projectContent[0].htmlText;
+        },
+        getImgSrcArr(state) {
+            return state.projectContent[0].imgSrcArr;
+        },
     },
     actions: {
 
