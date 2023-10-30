ad2e6ed1208443889e7f4931bab0bdb1109f0cea
Ivan Ustyantsev <vangold915@gmail.com>
2023-10-30 15:09:00 +0700

    articles and news block created and filled

---
+++ project-vue-cli-refactoring/src/components/pagination/PagintionWithRoutingComponent.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="pagination">
+      <PrimaryCircleButtonComponent
+        v-for="(page, index) in getQuantityOfPages"
+        :key="index"
+        :value="page"
+        :class="{ button_active: numberOfPage === page }"
+        @click="changePage(page)"
+      />
+      <PrimaryCircleButtonComponent @click="nextPage" />
+    </div>
+  </template>
+  
+  <script>
+  import PrimaryCircleButtonComponent from "../buttons/PrimaryCircleButtonComponent.vue";
+  
+  export default {
+    components: {
+      PrimaryCircleButtonComponent,
+    },
+    props: {
+      quantityElPerPage: {
+        type: Number,
+        default: () => 2,
+      },
+      total: {
+        type: Number,
+        default: () => 8,
+      },
+      numberOfPage: {
+        type: Number,
+        default: () => 1,
+      }
+    },
+    emits: ['page-changed'],
+    data() {
+      return {
+        currentPage: 1,
+      }
+    },
+    computed: {
+      getQuantityOfPages() {
+        return Math.ceil(this.total / this.quantityElPerPage);
+      },
+    },
+    methods: {
+      changePage(pageNumber) {
+        this.currentPage = pageNumber;
+        this.$emit('page-changed', pageNumber);
+      },
+      nextPage() {
+        this.currentPage = this.numberOfPage;
+        if (this.currentPage < this.getQuantityOfPages) {
+          this.currentPage += 1;
+          this.changePage(this.currentPage);
+        } 
+      },
+    },
+  }
+  </script>
+  
+  <style lang="scss" scoped>
+  .pagination {
+    display: flex;
+    gap: 20px;
+    justify-content: center;
+    // padding-top: 51px;
+    // padding-bottom: 104px;
+  }
+  </style>
+  
--- project-vue-cli-refactoring/src/pages/ArticlesAndNewsPage.vue
+++ project-vue-cli-refactoring/src/pages/ArticlesAndNewsPage.vue
@@ -21,54 +21,20 @@
         using.
       </p>
     </div>
-    <div id="articles__block">
-      <div class="articles__cards">
-        <ArticleCardComponent
-          v-for="article in getBlogArticles"
-          :key="article.id"
-          :settings="article"
-        />
-      </div>
-      <div class="articles__pagination">
-        <button
-          class="articles__pagination-link articles__pagination-link_active"
-          active
-        >
-          01
-        </button>
-        <button class="articles__pagination-link">02</button>
-        <button class="articles__pagination-link">03</button>
-        <button class="articles__pagination-link">
-          <svg
-            width="9"
-            height="16"
-            viewBox="0 0 9 16"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M1.77199 14.9527L7.71484 8.26698L1.77199 1.58127"
-              stroke="#292F36"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-          </svg>
-        </button>
-      </div>
-    </div>
+    <ArticlesAndNewsBlock />
   </section>
 </template>
 
 <script>
+import ArticlesAndNewsBlock from "@/blocks/articles/ArticlesAndNewsBlock.vue"
 import ArticleCardComponent from "@/components/cards/article-card/ArticleCardComponent.vue"
 import LatestPostComponent from "@/components/cards/article-card/LatestPostComponent.vue"
 import IntroBlock from "@/blocks/intro/IntroBlock.vue"
-
 import { mapGetters } from "vuex"
 
 export default {
   components: {
+    ArticlesAndNewsBlock,
     ArticleCardComponent,
     LatestPostComponent,
     IntroBlock,
@@ -83,7 +49,7 @@ export default {
     }
   },
   computed: {
-    ...mapGetters(["getBlogArticles", "getLatestPost"]),
+    ...mapGetters(["getLatestPost"]),
   },
 }
 </script>
@@ -134,33 +100,6 @@ export default {
     font-weight: 400;
     line-height: 150%; /* 33px */
     letter-spacing: 0.22px;
-  }
-  &__cards {
-    display: grid;
-    grid-template-columns: repeat(3, 382px);
-    gap: 27px;
-  }
-  &__pagination {
-    display: flex;
-    gap: 20px;
-    justify-content: center;
-    padding-top: 51px;
-    padding-bottom: 104px;
-  }
-  &__pagination-link {
-    width: 52px;
-    height: 52px;
-    border-radius: 50%;
-    border: 1px solid #cda274;
-    background-color: #fff;
-    transition: all 0.3s ease-in;
-    &:hover {
-      background-color: #f4f0ec;
-    }
-    &_active {
-      background-color: #f4f0ec;
-      border: none;
-    }
   }
 }
 </style>
